4bfdaea9f85987b48217cab3c57907c44db4e1b5
dansman805 <dansman805@gmail.com>
2017-11-01 19:50:13 -0400

    Added support for command line arguments

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import requests, bs4, bleach, pyperclip
+import requests, bs4, bleach, pyperclip, sys, getopt
 
-word = raw_input('What word do you want the definition for?\n')
+try:
+	word = sys.argv[1]
+except IndexError:
+	word = raw_input('What word do you want the definition for?\n')
+
 url = 'http://www.dictionary.com/browse/' + word
 res = requests.get(url)
 
-soup = bs4.BeautifulSoup(res.text, 'lxml')
+soup = bs4.BeautifulSoup(res.text, 'html5lib')
 
 #First definition is found at <div class="def-content">
 defContents = soup.select('.def-content')
